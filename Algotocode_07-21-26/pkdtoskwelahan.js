8a645c76fc5540fcd066b7e783122b43c085b4fa
Syron <syronleecabagua@gmail.com>
2026-07-22 00:25:06 +0800

    feat: upload JS drills to JS branch

--- Algotocode_07-21-26/pkdtoskwelahan.js
+++ Algotocode_07-21-26/pkdtoskwelahan.js
@@ -1,44 +1,31 @@
-// REAL LIFE EXAMPLE: Pagkadto sa Eskwelahan
-//
-// Kung adlaw sang klase kag may allowance,
-// tan-awon ko kung may gasolina ang motor.
-//
-// - Kung may gasolina -> masulod ko sakay motor.
-// - Kung wala gasolina pero may plete -> masulod ko sakay jeep.
-// - Kung wala gasolina kag wala plete -> indi ko kasulod.
-//
-// Pero kung wala allowance,
-// indi gid ko kasulod.
-//
-// Kung wala klase,
-// mapabilin lang ko sa balay.
-//
+// REAL LIFE EXAMPLE: KUNG MA ULAN
+// Kung ma ulan, ma dala ko payong 
+// Kung hindi, hindi ko mag dala payong bug at 
+// Pay kung mag dulom, ma dala ko japon payong
+// Pro kun sanag man, di ko mag dala payong
 
-function schoolDay() {
-    let mayKlase = true;
-    let mayAllowance = true;
-    let mayGasolina = false;
-    let mayPlete = true;
 
-    if (mayKlase) {
+let maUlan = true;
+let magDulom = true;
 
-        if (mayAllowance) {
+const readline = require("readline");
 
-            if (mayGasolina) {
-                console.log("Masulod ko sakay sang motor.");
-            } else if (mayPlete) {
-                console.log("Masulod ko sakay sang jeep.");
-            } else {
-                console.log("Indi ko kasulod kay wala gasolina kag wala plete.");
-            }
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
-        } else {
-            console.log("Indi ko kasulod kay wala allowance.");
-        }
+rl.question("Ga ulan da? ", (answer) => {
+    maUlan = answer === "huo";
 
-    } else {
-        console.log("Wala klase subong. Pahuway lang anay.");
-    }
+if (maUlan) {
+    console.log("Ma dala ko payong");
 }
-
-schoolDay();
+else {
+    rl.question("Ga dulom ba? ", (answer) => {
+         magDulom = answer === "huo";
+    if (magDulom) {
+        console.log("Ma dala gid ko japon payong");
+    } else {
+        console.log("Di ko mag dala payong bug at");
+    }})}})
